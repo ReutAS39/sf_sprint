8b4f9ae7a98c18cd561d8d7285a556fe477b9729
Chilly <reutas39@gmail.com>
2023-04-23 18:00:32 +0200

    POST SubmitData views modified

--- sprint/pereval/views.py
+++ sprint/pereval/views.py
@@ -1,32 +1,38 @@
-from rest_framework import viewsets, mixins
-from rest_framework.mixins import UpdateModelMixin, RetrieveModelMixin
+from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
 
 from pereval.models import PerevalAdded
 from pereval.serializers import PerevalSerializer
 
 
-class SubmitData(mixins.CreateModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
+class SubmitData(
+                mixins.CreateModelMixin,
+                mixins.RetrieveModelMixin,
+                viewsets.GenericViewSet,
+                mixins.UpdateModelMixin,
+                mixins.ListModelMixin
+                ):
     queryset = PerevalAdded.objects.all()
     serializer_class = PerevalSerializer
 
+    def get_queryset(self):
+        email = self.request.query_params.get('user__email', None)
+        pk = self.kwargs.get('pk')
+        if email:
+            return self.queryset.filter(user__email=email)
+        elif pk:
+            return self.queryset.filter(id=pk)
+        return self.queryset.none()
+
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        submit_data = self.get_object()
+
+        if submit_data.status != 'new':
+            return Response({'state': 0, 'message': 'Данные не могут быть отредактированы'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(submit_data, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
-
-    def perform_update(self, serializer):
-        serializer.save()
-
-    def partial_update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return self.update(request, *args, **kwargs)
+        return Response({'state': 1, 'message': 'Данные успешно отредактированы'})
