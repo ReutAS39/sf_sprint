a255eae7ca53c461661419aac0c270ad12f0064e
ReutAS <reutas39@gmail.com>
2025-03-25 10:42:01 +0200

    ref

---
+++ pereval/serializers.py
@@ -0,0 +1,125 @@
+from rest_framework import serializers
+
+from pereval.models import PerevalAdded, Coords, Level, Users, Images, PerevaladdedImages
+
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        verbose_name = 'Координаты'
+        exclude = ('id',)
+
+
+class LevelSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        exclude = ('id',)
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    data = serializers.URLField()
+
+    class Meta:
+        model = Images
+        fields = ('data', 'title')
+        verbose_name = 'Изображение'
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+
+    class Meta:
+        model = Users
+        fields = ('email', 'phone', 'fam', 'name', 'otc',)
+        verbose_name = 'Пользователь'
+
+
+class PerevalSerializer(serializers.ModelSerializer):
+    user = UsersSerializer()
+    coords = CoordsSerializer()
+    level = LevelSerialize()
+    images = ImagesSerializer(many=True)
+
+    class Meta:
+        model = PerevalAdded
+        fields = '__all__'
+
+    def create(self, validated_data):
+        users_data = validated_data.pop('user')
+        coords_data = validated_data.pop('coords')
+        image_data = validated_data.pop('images')
+        levels_data = validated_data.pop('level')
+        coords = Coords.objects.create(**coords_data)
+
+        if Level.objects.filter(winter=levels_data['winter'],
+                                summer=levels_data['summer'],
+                                autumn=levels_data['autumn'],
+                                spring=levels_data['spring']
+                                ).exists():
+            level = Level.objects.get(winter=levels_data['winter'],
+                                      summer=levels_data['summer'],
+                                      autumn=levels_data['autumn'],
+                                      spring=levels_data['spring']
+                                      )
+        else:
+            level = Level.objects.create(**levels_data)
+
+        if Users.objects.filter(email=users_data['email']).exists():
+            user = Users.objects.get(email=users_data['email'])
+        else:
+            user = Users.objects.create(**users_data)
+
+        pereval = PerevalAdded.objects.create(user=user, coords=coords, level=level, **validated_data)
+        for img in image_data:
+            image = Images.objects.create(**img)
+            PerevaladdedImages.objects.create(images=image, perevaladded=pereval)
+        return pereval
+
+    def update(self, instance, validated_data):
+
+        coords_data = validated_data.pop('coords')
+        image_data = validated_data.pop('images')
+        levels_data = validated_data.pop('level')
+        coords = instance.coords
+        level = instance.level
+
+        for img in image_data:
+            if Images.objects.filter(data=img['data'], title=img['title']).exists():
+                continue
+            else:
+                image = Images.objects.create(**img)
+                PerevaladdedImages.objects.create(images=image, perevaladded=instance)
+
+        coords.latitude = coords_data.get('latitude', coords.latitude)
+        coords.longitude = coords_data.get('longitude', coords.longitude)
+        coords.height = coords_data.get('height', coords.height)
+        coords.save()
+
+        level.winter = levels_data.get('winter', level.winter)
+        level.summer = levels_data.get('summer', level.summer)
+        level.autumn = levels_data.get('autumn', level.autumn)
+        level.spring = levels_data.get('spring', level.spring)
+        if Level.objects.filter(winter=levels_data['winter'],
+                                summer=levels_data['summer'],
+                                autumn=levels_data['autumn'],
+                                spring=levels_data['spring']
+                                ).exists():
+            instance.level = Level.objects.get(winter=levels_data['winter'],
+                                               summer=levels_data['summer'],
+                                               autumn=levels_data['autumn'],
+                                               spring=levels_data['spring']
+                                               )
+            instance.level.save()
+        else:
+            instance.level = Level.objects.create(winter=levels_data['winter'],
+                                                  summer=levels_data['summer'],
+                                                  autumn=levels_data['autumn'],
+                                                  spring=levels_data['spring'])
+            instance.level.save()
+
+        instance.beauty_title = validated_data.get('beauty_title', instance.beauty_title)
+        instance.title = validated_data.get('title', instance.title)
+        instance.other_titles = validated_data.get('other_titles', instance.other_titles)
+        instance.connect = validated_data.get("connect", instance.connect)
+        instance.save()
+        return instance
---
+++ pereval/views.py
@@ -0,0 +1,48 @@
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+
+from pereval.models import PerevalAdded
+from pereval.serializers import PerevalSerializer
+
+
+class SubmitData(
+                mixins.CreateModelMixin,
+                mixins.RetrieveModelMixin,
+                viewsets.GenericViewSet,
+                mixins.UpdateModelMixin,
+                mixins.ListModelMixin
+                ):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalSerializer
+    http_method_names = ['get', 'post', 'patch']
+
+    def get_queryset(self):
+        email = self.request.query_params.get('user__email', None)
+        pk = self.kwargs.get('pk')
+        if email:
+            return self.queryset.filter(user__email=email)
+        elif pk:
+            return self.queryset.filter(id=pk)
+        return self.queryset.none()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+
+        submit_data = self.get_object()
+
+        if submit_data.status != 'new':
+            return Response({'state': 0, 'message': 'Данные не могут быть отредактированы'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(submit_data, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response({'state': 1, 'message': 'Данные успешно отредактированы', "Перевал": serializer.data},
+                        status=status.HTTP_204_NO_CONTENT)
