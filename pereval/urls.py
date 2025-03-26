583994ca5e5a1c6277048f2c5a1e45d3e8481cbb
ReutAS <reutas39@gmail.com>
2025-03-26 16:06:03 +0200

    urls upd

--- pereval/urls.py
+++ pereval/urls.py
@@ -3,7 +3,7 @@ from .views import PerevalDetail, PerevalList, PerevalAdd, PerevalUpdate
 
 urlpatterns = [
     path('submitData/', PerevalAdd.as_view(), name='pereval-add'),
-    path('submitData/<int:id>/', PerevalDetail.as_view(), name='pereval-detail'),
+    path('submitData/<int:id>', PerevalDetail.as_view(), name='pereval-detail'),
     path('submitData/<int:id>', PerevalUpdate.as_view(), name='pereval-update'),
-    path('submitData/<str:email>/', PerevalList.as_view(), name='pereval-list'),
+    path(r'submitData/<str:email>', PerevalList.as_view(), name='pereval-list'),
 ]
